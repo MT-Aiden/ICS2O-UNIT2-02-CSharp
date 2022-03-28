f89a154515428b0ddba3170d0923c0b819f6fd25
MT-Aiden <99355180+MT-Aiden@users.noreply.github.com>
2022-03-28 10:12:33 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,18 @@
+// Created by: Aiden McLeod
+// Created on: Sep 2020
+// This program does basic math
+
+using System;
+
+public class Program 
+{
+
+    public static void Main(string[] args) 
+    {
+        Console.WriteLine("The dimensions of a rectangle are: 5cm and 3cm");
+        Console.WriteLine("The area is " + (5 * 3));
+        Console.WriteLine("The perimeter is " + (2 * (5 + 3)));
+
+        Console.WriteLine("\nDone.");
+    }
+}
